c0953d2f4bb42d13fd608a74c64d8db0988a7f29
amilaupendra <amilaupendra5@gmailcom>
2024-03-12 13:35:04 +0530

    started emergent labs web app

---
+++ app/pages/home/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar/page'
+
+const homepage = () => {
+  return (
+    
+<div>
+    <Navbar/>
+    </div>
+  )
+}
+
+export default homepage
