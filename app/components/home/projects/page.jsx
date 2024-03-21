1a0864b4beb879689104108662addad81849f4bc
amilaupendra <amilaupendra5@gmailcom>
2024-03-21 21:40:05 +0530

    ideas added

--- app/components/home/projects/page.jsx
+++ app/components/home/projects/page.jsx
@@ -7,22 +7,22 @@ const projects = () => {
       <h1 className='pt-12 pb-6 text-3xl font-bold'>Want new ideas?</h1>
         <div className="carousel rounded-box">
   <div className="p-3 carousel-item">
-    <Image className='hover:scale-125' src="/cake.WEBP" width={500} height={500} alt="Burger" />
+    <Image className='hover:scale-125' src="/cake.jpg" width={500} height={500} alt="Burger" />
   </div>
   <div className="p-3 carousel-item">
-    <Image className='hover:scale-125' src="/furniture.WEBP" width={500} height={500} alt="Burger" />
+    <Image className='hover:scale-125' src="/furniture.jpg" width={500} height={500} alt="Burger" />
   </div> 
   <div className="p-3 carousel-item">
-    <Image className='hover:scale-125' src="/craft.WEBP" width={500} height={500} alt="Burger" />
+    <Image className='hover:scale-125' src="/craft.jpg" width={500} height={500} alt="Burger" />
   </div> 
   <div className="p-3 carousel-item">
-    <Image className='hover:scale-125' src="/portfolio.WEBP"  width={500} height={500} alt="Burger" />
+    <Image className='hover:scale-125' src="/portfolio.jpg"  width={500} height={500} alt="Burger" />
   </div> 
   <div className="p-3 carousel-item">
-    <Image className='hover:scale-125' src="/coffee.WEBP" width={500} height={500} alt="Burger" />
+    <Image className='hover:scale-125' src="/coffee.jpg" width={500} height={500} alt="Burger" />
   </div> 
   <div className="p-3 carousel-item">
-    <Image className='hover:scale-125' src="/crafts2.WEBP" width={500} height={500} alt="Burger" />
+    <Image className='hover:scale-125' src="/crafts2.jpg" width={500} height={500} alt="Burger" />
   </div> 
   
 </div>
