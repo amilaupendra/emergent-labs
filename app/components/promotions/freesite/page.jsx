7528e60f81a1df79f581cfc2cf86a9e3cd929de0
amilaupendra <amilaupendra5@gmailcom>
2024-04-07 02:54:05 +0530

    abc

--- app/components/promotions/freesite/page.jsx
+++ app/components/promotions/freesite/page.jsx
@@ -212,7 +212,7 @@ const page = () => {
                 htmlFor="email"
                 className="block mb-2 text-xs font-bold tracking-wide text-gray-700 uppercase"
               >
-                Do you need additional requirements?  
+                Do you have additional requirements?  
               </label>
               <input
                 className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
